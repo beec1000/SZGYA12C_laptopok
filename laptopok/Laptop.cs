7c430aa7e6eaec93f762a7b606553104e237ec5e
beec1000 <beec1000.minecraft@gmail.com>
2023-12-04 20:33:10 +0100

    finished

--- laptopok/Laptop.cs
+++ laptopok/Laptop.cs
@@ -9,21 +9,33 @@ namespace laptopok
     public class Laptop
     {
         public int Azonosito { get; set; }
-        public string Tipus { get; set; }
-        public int Sebesseg { get; set; }
+        public string Processzor { get; set; }
         public string OS { get; set; }
-        public string Gyarto { get; set; }
-        public string Modell { get; set; }
+        public string GyartoModell { get; set; }
         public double AkkuIdeje /*ÓRA*/ { get; set; }
-        public double Súly /*KG*/ { get; set; }
+        public double Suly /*KG*/ { get; set; }
         public string[] VezetekNelkuli { get; set; }
+
+        //12. feladat
+        public double SulyG => Suly * 1000;
+
 
         public Laptop(string s)
         {
             var v = s.Split('|');
             this.Azonosito = int.Parse(v[0]);
-            var processzor = v[1].Split(" ");
-            
+            this.Processzor = v[1];
+            this.OS = v[2];
+            this.GyartoModell = v[3];
+            this.AkkuIdeje = double.Parse(v[4]);
+            this.Suly = double.Parse(v[5]);
+            this.VezetekNelkuli = v[6].Split(", ");
+
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Azonosito}|{this.Processzor}|{this.OS}|{string.Join(", ", this.GyartoModell)}|{this.AkkuIdeje}|{this.Suly}|{string.Join(", ", this.VezetekNelkuli)}";
         }
     }
 }
--- laptopok/Program.cs
+++ laptopok/Program.cs
@@ -1,7 +1,38 @@
-﻿namespace laptopok
+﻿using System.Linq;
+
+namespace laptopok
 {
     public class Program
     {
+        static List<Laptop> F7(List<Laptop> l) 
+        { 
+            return l.Where(d => !d.OS.Contains("Windows") && d.Suly >= 2).ToList();
+        }
+
+        static List<Laptop> F8(List<Laptop> l) 
+        { 
+            var x = l.Min(d => d.AkkuIdeje);
+            return l.Where(d => d.AkkuIdeje == x).ToList();
+        }
+
+        static double F9(List<Laptop> l) 
+        { 
+            return l.Average(d => double.Parse(d.Processzor.Split(" ").Last()));
+        }
+
+        static List<string> F10(List<Laptop> l)
+        {
+            var x = l.Where(d => d.VezetekNelkuli.Contains("Bluetooth 5.2"));
+            var y = x.Select(d => d.GyartoModell.Split(" ").First()).Distinct().OrderBy(d => d).ToList();
+            return y;
+        }
+
+        static List<Laptop> F11(List<Laptop> l)
+        {
+            var x = l.Where(d => d.Processzor.Contains("AMD") && d.Suly > 1.3).ToList();
+            return x;
+        }
+
         static void Main(string[] args)
         {
             var laptopok = new List<Laptop>();
@@ -13,6 +44,66 @@
                 while (!sr.EndOfStream)
                 {
                     laptopok.Add(new Laptop(sr.ReadLine()));
+                }
+            }
+
+            foreach (var i in laptopok)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("7. feladat");
+            var laptopokF7 = new List<Laptop>(laptopok);
+            var f7 = F7(laptopokF7);
+            Console.WriteLine($"{f7.Count}db olyan laptop van ami több mint 2kg és nem Windows van rajta.");
+
+            Console.WriteLine("8. feladat");
+            var f8 = F8(laptopok);
+            foreach (var i in f8)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine($"{f8.Count}db ilyen laptop van, legkisebb akku idő: {f8.First().AkkuIdeje}");
+
+            Console.WriteLine("9. feladat");
+            var f9 = F9(laptopok);
+            Console.WriteLine($"Az átlag processzor sebesség: {f9}");
+
+            Console.WriteLine("10. feladat");
+            var f10 = F10(laptopok);
+            if (f10.Count != null)
+            {
+                foreach (var i in f10)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            else
+            {
+                Console.WriteLine("HIBA! 404");
+            }
+
+            Console.WriteLine("11. feladat");
+            var f11 = F11(laptopok);
+            Console.WriteLine("1,3kg súlynál nehezebb AMD-s laptopok azonositói: ");
+            if (f11 != null)
+            {
+                foreach (var i in f11)
+                {
+                    Console.WriteLine(i.Azonosito);
+                }
+            }
+            else
+            {
+                Console.WriteLine("HIBA! 404");
+            }
+
+            //13. feladat
+            using var sw = new StreamWriter(@"..\..\..\src\laptopok2.txt");
+            {
+                foreach (var i in laptopok)
+                {
+                    sw.WriteLine($"{i.Processzor}|{i.SulyG}");
                 }
             }
 
