badf39c94bbe817eb464876ddc7c87cd5728688c
beec1000 <beec1000.minecraft@gmail.com>
2023-12-04 14:05:35 +0100

    Add project files.

---
+++ laptopok/Laptop.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laptopok
+{
+    public class Laptop
+    {
+        public int Azonosito { get; set; }
+        public string Tipus { get; set; }
+        public int Sebesseg { get; set; }
+        public string OS { get; set; }
+        public string Gyarto { get; set; }
+        public string Modell { get; set; }
+        public double AkkuIdeje /*ÓRA*/ { get; set; }
+        public double Súly /*KG*/ { get; set; }
+        public string[] VezetekNelkuli { get; set; }
+
+        public Laptop(string s)
+        {
+            var v = s.Split('|');
+            this.Azonosito = int.Parse(v[0]);
+            var processzor = v[1].Split(" ");
+            
+        }
+    }
+}
---
+++ laptopok/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace laptopok
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var laptopok = new List<Laptop>();
+
+            using var sr = new StreamReader(@"..\..\..\src\laptopok.txt");
+            {
+                _ = sr.ReadLine();
+
+                while (!sr.EndOfStream)
+                {
+                    laptopok.Add(new Laptop(sr.ReadLine()));
+                }
+            }
+
+        }
+    }
+}
